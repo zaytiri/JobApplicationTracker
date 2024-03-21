007740fb4eb959587b82b8efc1ff91f36f268fea
zay <64491586+zaytiri@users.noreply.github.com>
2024-03-21 22:32:00 +0000

    fix issue with server not being able to be exited;

--- app/api/src/main/java/org/example/Main.java
+++ app/api/src/main/java/org/example/Main.java
@@ -1,25 +1,25 @@
 package org.example;
 
+import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
 import java.net.URI;
 
 public class Main {
 
     public static final String BASE_URI = "http://localhost:8080/";
 
-    public static void main(String[] args) {
+    public static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig().packages("org.example");
-        final URI uri = URI.create(BASE_URI);
-        final org.glassfish.grizzly.http.server.HttpServer server =
-                GrizzlyHttpServerFactory.createHttpServer(uri, rc);
-        System.out.println("Server started at " + BASE_URI);
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            server.shutdownNow();
-        }
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.printf("Listening...%s", BASE_URI);
+        System.in.read();
+        server.shutdownNow();
     }
 }
