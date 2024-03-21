dce3969207ef1f5f1196f91c1d079c4941b623cd
zay <64491586+zaytiri@users.noreply.github.com>
2024-03-21 18:41:34 +0000

    update React render

--- app/ui/src/js/index.js
+++ app/ui/src/js/index.js
@@ -1,4 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { MyComponent } from "./api_calls";
 
-ReactDOM.render(<h1>Hello React App</h1>, document.getElementById('root'))
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<MyComponent/>);
