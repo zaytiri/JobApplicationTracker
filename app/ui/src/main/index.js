53c6968c06a70382ee0363ba07345a902772b051
zay <64491586+zaytiri@users.noreply.github.com>
2024-03-26 19:34:21 +0000

    add react-bootstrap, bootstract and sass suport

--- app/ui/src/main/index.js
+++ app/ui/src/main/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { JobTracker } from "./job_tracker";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
