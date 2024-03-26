9d31ca04a465f4a7c635c152dfaeeb890b9c8051
zay <64491586+zaytiri@users.noreply.github.com>
2024-03-26 13:22:48 +0000

    update package where rest controllers can be found

--- app/api/src/main/java/personal/zaytiri/jobtracker/Main.java
+++ app/api/src/main/java/personal/zaytiri/jobtracker/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static final String BASE_URI = "http://localhost:8080/";
 
     public static HttpServer startServer() {
-        final ResourceConfig rc = new ResourceConfig().packages("org.example");
+        final ResourceConfig rc = new ResourceConfig().packages("personal.zaytiri.jobtracker.api.controllers");
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
