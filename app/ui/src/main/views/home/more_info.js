4d8a4821f4138eacb0703c8630e1c78b90843947
zay <64491586+zaytiri@users.noreply.github.com>
2024-04-03 00:10:41 +0100

    refactor more info component design

--- app/ui/src/main/views/home/more_info.js
+++ app/ui/src/main/views/home/more_info.js
@@ -8,16 +8,23 @@ import {
   Button,
   Spacer,
   Collapse,
+  Avatar,
+  Box,
+  Image,
+  useColorMode,
+  Link,
 } from "@chakra-ui/react";
 // Custom components
 import Card from "../../template/components/Card/Card.js";
 import CardBody from "../../template/components/Card/CardBody.js";
 import CardHeader from "../../template/components/Card/CardHeader.js";
+import bgAdmin from "../../template/assets/img/admin-background.png";
 
 import { remove } from "../../api/api_endpoints/job_offer_api.js"
 
-export const MoreInfo = ({currentJobOffer, jobOffers, setJobOffers, onToggle}) => {
+export const MoreInfo = ({ currentJobOffer, currentStatus, jobOffers, setJobOffers, onToggle }) => {
   const textColor = useColorModeValue("gray.700", "white");
+  const { colorMode } = useColorMode();
 
   const formatDescription = (text) => {
     if (typeof text === 'undefined') {
@@ -39,113 +46,142 @@ export const MoreInfo = ({currentJobOffer, jobOffers, setJobOffers, onToggle}) =
 
   const removeJobOffer = async (id) => {
     const response = await remove(id);
-    
+
     if (response.success === false) return null;
 
     onToggle();
-    setJobOffers(jobOffers.filter((jobOffer) => jobOffer.id !== id ));
+    setJobOffers(jobOffers.filter((jobOffer) => jobOffer.id !== id));
   }
 
-    return (
-        <Card p='15px' maxW={{ sm: "320px", md: "100%" }} h="100%">
-            <CardHeader p='12px 5px' mb='12px'>
-              <Flex >
-                <Text fontSize='lg' color={textColor} fontWeight='bold'>
-                  {currentJobOffer.company}
-                </Text>
-                <Spacer />
-                <Badge
-                  bg="green"
-                  color="black"
-                  fontSize="16px"
-                  p="3px 10px"
-                  borderRadius="8px">
-                  {currentJobOffer.status}
-                </Badge>
-              </Flex>
-            </CardHeader>
-            <CardBody px='5px' height="100%">
-              <Flex direction='column' height="100%">
-                <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                  <Collapse startingHeight={50} in={show} key={currentJobOffer.id}>
-                    {formatDescription(currentJobOffer.description)}
-                  </Collapse>
-                  <Button size='sm' onClick={handleToggleToShowDescription} mt='1rem'>
-                    Show {show ? 'Less' : 'More'}
-                  </Button>
-                </Text>
-                <Flex mb='18px' direction="column">
-                  <Text
-                    fontSize='md'
-                    color={textColor}
-                    fontWeight='bold'
-                    me='10px'>
-                    Role:{" "}
-                  </Text>
-                  <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                    {currentJobOffer.role}
-                  </Text>
-                </Flex>
-                <Flex mb='18px' direction="column">
-                  <Text
-                    fontSize='md'
-                    color={textColor}
-                    fontWeight='bold'
-                    me='10px'>
-                    Location:{" "}
-                  </Text>
-                  <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                    {currentJobOffer.location}
-                  </Text>
-                </Flex>
-                <Flex mb='18px' direction="column">
-                  <Text
-                    fontSize='md'
-                    color={textColor}
-                    fontWeight='bold'
-                    me='10px'>
-                    Company Website:{" "}
-                  </Text>
-                  <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                    {currentJobOffer.companyWebsite}
-                  </Text>
-                </Flex>
-                <Flex mb='18px' direction="column">
-                  <Text
-                    fontSize='md'
-                    color={textColor}
-                    fontWeight='bold'
-                    me='10px'>
-                    Job Offer Link:{" "}
-                  </Text>
-                  <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                    {currentJobOffer.link}
-                  </Text>
-                </Flex>
-                <Flex mb='18px' direction="column">
-                  <Text
-                    fontSize='md'
-                    color={textColor}
-                    fontWeight='bold'
-                    me='10px'>
-                    Interview Notes:{" "}
-                  </Text>
-                  <Text fontSize='md' color='gray.400' fontWeight='400' mb='30px' style={{ whiteSpace: 'wrap' }}>
-                    {currentJobOffer.interviewNotes}
-                  </Text>
-                </Flex>
-                <Spacer />
-                <Flex>
-                  <Button variant="primary">
-                    EDIT
-                  </Button>
-                  <Spacer />
-                  <Button variant="danger" onClick={() => removeJobOffer(currentJobOffer.id)}>
-                    DELETE
-                  </Button>
-                </Flex>
-              </Flex>
-            </CardBody>
-          </Card>
-    )
+  const hexToRgb = (hex) => {
+    // Remove the leading '#' if present
+    hex = hex.replace(/^#/, '');
+
+    // Parse the hex color to get RGB values
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return { r, g, b };
+  }
+
+  const hexToRgba = (hex, opacity) => {
+    const { r, g, b } = hexToRgb(hex);
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  }
+
+  return (
+    <Card p='15px' maxW={{ sm: "320px", md: "100%" }} h="100%" w='100%'>
+      <CardHeader mb={{ base: "0px", lg: "20px" }} align='center'>
+        <Box
+          bgColor={hexToRgba(currentStatus === undefined ? '#FFFFFF' : currentStatus.color, 0.2)}
+          borderRadius='16px'
+          h='160px'
+          w='100%'
+          boxShadow='0px 7px 18px 3px rgba(0, 0, 0, 0.2)'
+        >
+          <Flex p='10px' alignItems='center'>
+            <Button variant="primary" >
+              Get updates
+            </Button>
+            <Spacer />
+            <Button variant="primary">
+              EDIT
+            </Button>
+          </Flex>
+        </Box>
+
+        <Flex mx='auto'
+          h='100%'
+          w='100%'
+          mt='-66px'
+          align='center'
+          direction='column'>
+
+          <Text fontSize='2xl' color={textColor} fontWeight='bold'>
+            {currentJobOffer.company}
+          </Text>
+          <Text fontSize='lg' color={textColor} >
+            {currentJobOffer.role}
+          </Text>
+          <Badge
+            bg={currentStatus === undefined ? 'white' : currentStatus.color}
+            color="black"
+            fontSize="16px"
+            borderRadius="8px"
+          >
+            {currentStatus !== undefined && currentStatus.name}
+            {currentStatus === undefined && 'No Status'}
+          </Badge>
+        </Flex>
+      </CardHeader>
+
+      <CardBody px='5px' height="100%">
+        <Flex direction='column' height="100%">
+          <Button variant="primary" mt='15px' mb='15px'>
+            <Link href={currentJobOffer.link} isExternal>
+              Go to Job Offer
+            </Link>
+          </Button>
+          <Flex mb='10px' direction="column">
+            <Text
+              fontSize='md'
+              color={textColor}
+              fontWeight='bold'
+              me='10px'>
+              Location:{" "}
+            </Text>
+            <Text fontSize='md' color='gray.400' fontWeight='400' mb='15px' style={{ whiteSpace: 'wrap' }}>
+              {currentJobOffer.location}
+            </Text>
+          </Flex>
+          <Flex mb='10px' direction="column">
+            <Text
+              fontSize='md'
+              color={textColor}
+              fontWeight='bold'
+              me='10px'>
+              Company Website:{" "}
+            </Text>
+            <Text fontSize='md' color='gray.400' fontWeight='400' mb='15px' style={{ whiteSpace: 'wrap' }}>
+              {currentJobOffer.companyWebsite}
+            </Text>
+          </Flex>
+          <Flex mb='10px' direction="column">
+            <Text
+              fontSize='md'
+              color={textColor}
+              fontWeight='bold'
+              me='10px'>
+              Interview Notes:{" "}
+            </Text>
+            <Text fontSize='md' color='gray.400' fontWeight='400' mb='15px' style={{ whiteSpace: 'wrap' }}>
+              {currentJobOffer.interviewNotes}
+            </Text>
+          </Flex>
+          <Text fontSize='md' color='gray.400' fontWeight='400' mb='10px' style={{ whiteSpace: 'wrap' }}>
+            <Text
+              fontSize='md'
+              color={textColor}
+              fontWeight='bold'
+              me='10px'>
+              Description:{" "}
+            </Text>
+            <Collapse startingHeight={50} in={show} key={currentJobOffer.id}>
+              {formatDescription(currentJobOffer.description)}
+            </Collapse>
+            <Button size='sm' onClick={handleToggleToShowDescription} mt='1rem'>
+              Show {show ? 'Less' : 'More'}
+            </Button>
+          </Text>
+          <Flex justifyContent='right'>
+            <Button variant="danger" onChange={() => removeJobOffer(currentJobOffer.id)}>
+              DELETE
+            </Button>
+          </Flex>
+        </Flex>
+      </CardBody>
+    </Card>
+  )
 }
