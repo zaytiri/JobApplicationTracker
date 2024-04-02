d3dd0a828885d5b11e8dbf7facfbb4e842a1f440
zay <64491586+zaytiri@users.noreply.github.com>
2024-04-02 16:16:23 +0100

    add job offer api endpoints

---
+++ app/ui/src/main/api/api_endpoints/job_offer_api.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import axios from "axios";
+import { BASE_URI } from "../api_configs";
+
+const baseUrl = BASE_URI + "job-offer/";
+const headers = {
+  headers: {
+    'Content-Type': 'text/plain'
+  }
+}
+
+export const create = async function(jobOffer) {
+  return await axios.post(baseUrl + "create", jobOffer, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const get = async function(filters) {
+  return await axios.post(baseUrl + "get", filters, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const getById = async function(id) {
+  var filters = {
+    "id": {"=": id}
+  }
+  return await axios.post(baseUrl + "get", filters, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const update = async function(id, jobOffer) {
+  return await axios.put(baseUrl + "update/" + id, jobOffer, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const remove = async function(id) {
+  return await axios.delete(baseUrl + "remove/" + id, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const scrape = async function(url) {
+  return await axios.post(baseUrl + "scrape", url, headers).then((response) => {
+    return response.data;
+  });
+};
+
+export const getJobOffersByStatus = async function() {
+  return await axios.get(baseUrl + "job-offer-by-status", headers).then((response) => {
+    return response.data;
+  });
+};
---
+++ app/ui/src/main/api/entities/job_offer.js
@@ -0,0 +1,31 @@
+
+export const createJobOfferObject = function(props) {
+
+    const { 
+        company, 
+        role, 
+        companyWebsite, 
+        location,
+        link,
+        description,
+        applied,
+        interviewNotes,
+        statusId } = props;
+    
+    let appliedAt = null;
+    if(applied){
+        appliedAt = new Date();
+    }
+
+    return {
+        company: company,
+        role: role,
+        companyWebsite: companyWebsite,
+        location: location,
+        link: link,
+        description: description,
+        appliedAt: appliedAt,
+        statusId: statusId.toString(),
+        interviewNotes: interviewNotes,
+    }
+}
