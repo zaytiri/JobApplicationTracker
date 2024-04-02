e5930e6148a805e4b4cca1a13151ef5c193d228d
zay <64491586+zaytiri@users.noreply.github.com>
2024-04-02 16:13:25 +0100

    add interface template from creative-tim
    URL: https://www.creative-tim.com/product/argon-dashboard-chakra?ref=docs-argonchakra

---
+++ app/ui/src/main/template/components/Tables/JobApplicationTableRow.js
@@ -0,0 +1,92 @@
+import {
+  Avatar,
+  Badge,
+  Button,
+  Flex,
+  Td,
+  Text,
+  Tr,
+  useColorModeValue
+} from "@chakra-ui/react";
+import React from "react";
+import { formatDistanceToNow } from 'date-fns';
+
+function JobApplicationTableRow(props) {
+  const { company, role, location, status, applied, isLast, moreAction } = props;
+  
+  const textColor = useColorModeValue("gray.500", "white");
+  const titleColor = useColorModeValue("gray.700", "white");
+  const borderColor = useColorModeValue("gray.200", "gray.600");
+
+  const formatNumber = (num) => {
+    return num < 10 ? `0${num}` : num;
+  };
+
+  return (
+    <Tr>
+      <Td
+        pl="0px"
+        borderColor={borderColor}
+        borderBottom={isLast ? "none" : null}>
+        <Text fontSize="md" color={textColor} fontWeight="bold">
+            {company}
+          </Text>
+      </Td>
+      <Td borderColor={borderColor} borderBottom={isLast ? "none" : null}>
+        <Flex direction="column">
+          <Text fontSize="md" color={textColor} fontWeight="bold">
+            {role}
+          </Text>
+        </Flex>
+      </Td>
+      <Td borderColor={borderColor} borderBottom={isLast ? "none" : null}>
+      <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+          {location}
+        </Text>
+      </Td>
+      <Td borderColor={borderColor} borderBottom={isLast ? "none" : null}>
+      <Flex align="center" py=".8rem" minWidth="100%" flexWrap="nowrap">
+          <Flex direction="column">
+            <Text
+              fontSize="md"
+              color={titleColor}
+              fontWeight="bold"
+              minWidth="100%"
+            >
+              {applied[0] > 0 && applied[0] + '-' + formatNumber(applied[1]) + '-' + formatNumber(applied[2])}
+              {applied[0] < 0 && 'Not yet Applied'}
+            </Text>
+            <Text fontSize="sm" color="gray.400" fontWeight="normal">
+              {applied[0] > 0 && 'Applied ' + formatDistanceToNow(new Date(applied[0], applied[1] - 1, applied[2], applied[3], applied[4], applied[5]), { addSuffix: true, includeSeconds: true })}
+            </Text>
+          </Flex>
+        </Flex>
+      </Td>
+      <Td borderColor={borderColor} borderBottom={isLast ? "none" : null}>
+        <Badge
+          bg={status === undefined ? 'white' : status.color}
+          color="black"
+          fontSize="16px"
+          p="3px 10px"
+          borderRadius="8px"
+        >
+          {status !== undefined && status.name}
+          {status === undefined && 'No Status'}
+        </Badge>
+      </Td>
+      <Td borderColor={borderColor} borderBottom={isLast ? "none" : null}>
+        <Button p="0px" bg="transparent" variant="no-effects" onClick={moreAction}>
+          <Text
+            fontSize="md"
+            color="gray.400"
+            fontWeight="bold"
+            cursor="pointer">
+            More
+          </Text>
+        </Button>
+      </Td>
+    </Tr>
+  );
+}
+
+export default JobApplicationTableRow;
---
+++ app/ui/src/main/template/routes.js
@@ -0,0 +1,86 @@
+// import
+import React from 'react';
+import Dashboard from "./views/Dashboard/Dashboard.js";
+import Tables from "./views/Dashboard/Tables.js";
+import Billing from "./views/Dashboard/Billing.js";
+import Profile from "./views/Dashboard/Profile.js";
+import { Home } from '../views/home/home.js';
+import { DraggableView } from '../views/draggable_view/draggable_view.js';
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+} from "./components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: <CreditIcon color='inherit' />,
+    component: Home,
+    layout: "/home",
+  },
+  {
+    path: "/draggableview",
+    name: "Draggable View",
+    icon: <CreditIcon color='inherit' />,
+    component: DraggableView,
+    layout: "/home",
+  },
+  {
+    path: "/statistics",
+    name: "Statistics",
+    icon: <CreditIcon color='inherit' />,
+    component: Dashboard,
+    layout: "/home",
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    icon: <CreditIcon color='inherit' />,
+    component: DraggableView,
+    layout: "/home",
+  },
+  {
+    name: "TEMPORARY TEMPLATES",
+    category: "account",
+    state: "pageCollapse",
+    views: [
+      {
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: <HomeIcon color='inherit' />,
+        secondaryNavbar: true,
+        component: Dashboard,
+        layout: "/home",
+      },
+      {
+        path: "/tables",
+        name: "Tables",
+        icon: <StatsIcon color='inherit' />,
+        secondaryNavbar: true,
+        component: Tables,
+        layout: "/home",
+      },
+      {
+        path: "/billing",
+        name: "Billing",
+        icon: <CreditIcon color='inherit' />,
+        secondaryNavbar: true,
+        component: Billing,
+        layout: "/home",
+      },
+      {
+        path: "/profile",
+        name: "Profile",
+        icon: <PersonIcon color='inherit' />,
+        secondaryNavbar: true,
+        component: Profile,
+        layout: "/home",
+      },
+    ],
+  },
+];
+export default dashRoutes;
