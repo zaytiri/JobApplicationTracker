38617aeca3a6554ad10c51404b629ed0dcaef86b
zay <64491586+zaytiri@users.noreply.github.com>
2024-05-07 00:06:22 +0100

    remove unnecessary navbar buttons;

--- app/ui/src/main/template/components/Navbars/AdminNavbarLinks.js
+++ app/ui/src/main/template/components/Navbars/AdminNavbarLinks.js
@@ -79,7 +79,7 @@ export default function HeaderLinks(props) {
         routes={routes}
         {...rest}
       />
-      <SettingsIcon
+      {/* <SettingsIcon
         cursor='pointer'
         ms={{ base: "16px", xl: "0px" }}
         me='16px'
@@ -87,43 +87,14 @@ export default function HeaderLinks(props) {
         color={navbarIcon}
         w='18px'
         h='18px'
-      />
-      <Menu>
+      /> */}
+      {/* <Menu>
         <MenuButton>
           <BellIcon color={navbarIcon} w='18px' h='18px' />
         </MenuButton>
         <MenuList p='16px 8px' bg={menuBg}>
-          <Flex flexDirection='column'>
-            <MenuItem borderRadius='8px' mb='10px'>
-              <ItemContent
-                time='13 minutes ago'
-                info='from Alicia'
-                boldInfo='New Message'
-                aName='Alicia'
-                aSrc={avatar1}
-              />
-            </MenuItem>
-            <MenuItem borderRadius='8px' mb='10px'>
-              <ItemContent
-                time='2 days ago'
-                info='by Josh Henry'
-                boldInfo='New Album'
-                aName='Josh Henry'
-                aSrc={avatar2}
-              />
-            </MenuItem>
-            <MenuItem borderRadius='8px'>
-              <ItemContent
-                time='3 days ago'
-                info='Payment succesfully completed!'
-                boldInfo=''
-                aName='Kara'
-                aSrc={avatar3}
-              />
-            </MenuItem>
-          </Flex>
         </MenuList>
-      </Menu>
+      </Menu> */}
     </Flex>
   );
 }
